0c546ea1bb52a34cd94f34e3c0f893d1d93a5c5a
z0uh1r-dev <zouhir.amahzoune@gmail.com>
2022-05-28 07:21:40 +0100

    Add posts count

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const IndexPage = ({ data }) => {
       <Seo title="Home" />
       <div>
         <h1>Zouhir's Thoughts</h1>
-        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
 
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
